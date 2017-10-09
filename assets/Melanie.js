2e13949b548b12bbfa9bd7176dfb7fcacd635ecc
Melanie West <dhmofan@gmail.com>
2017-10-09 16:40:22 -0400

    Basic js added to my js file; links created for everyone, though not all js files are created yet

---
+++ assets/Melanie.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+           
+            var imgMelanie = $('<img>');
+            var nameMelanie = $('<p>');
+            imgMelanie.attr("src",imgURL);
+            nameMelanie.html('Melanie West');
+            
+            $('#Melanie_West').append(imgMelanie);
+            $('#Melanie_West').append(nameMelanie);
+            
+});
