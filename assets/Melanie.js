971360e63d10a9983db96f31a864e86286d9a24f
Melanie West <dhmofan@gmail.com>
2017-10-09 17:13:14 -0400

    links to js files completed.  Some js files not yet created.  Add two more links for Josh and Justin

--- assets/Melanie.js
+++ assets/Melanie.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
            
             var imgMelanie = $('<img>');
             var nameMelanie = $('<p>');
-            imgMelanie.attr("src",imgURL);
+            var imgUrlMel = "https://media.giphy.com/media/g9582DNuQppxC/giphy.gif"
+            imgMelanie.attr("src",imgUrlMel);
             nameMelanie.html('Melanie West');
             
             $('#Melanie_West').append(imgMelanie);
