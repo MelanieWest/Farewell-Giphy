196238a9278a6b4850735f986096773183e098f5
Melanie West <dhmofan@gmail.com>
2017-10-11 09:06:24 -0400

    modify giphy

--- assets/Melanie.js
+++ assets/Melanie.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
            
             var imgMelanie = $('<img>');
             var nameMelanie = $('<p>');
-            var imgUrlMel = "https://media.giphy.com/media/g9582DNuQppxC/giphy.gif"
+           // https://media.giphy.com/media/Hl1PqpoxTVGdG/giphy.gif
+            var imgUrlMel = "https://media.giphy.com/media/Hl1PqpoxTVGdG/giphy.gif"           
+            //var imgUrlMel = "https://media.giphy.com/media/g9582DNuQppxC/giphy.gif"
             imgMelanie.attr("src",imgUrlMel);
             nameMelanie.html('Melanie West');
             
