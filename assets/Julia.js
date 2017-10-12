e8a3d7d1fc4a374d709246295be30d4c7f527284
Julia McGee <juliaangeletti@gmail.com>
2017-10-12 08:43:14 -0400

    Update Julia.js

--- assets/Julia.js
+++ assets/Julia.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
             var imgJulia = $('<img>');
             var nameJulia = $('<p>');
             var imgUrlJulia = "https://media.giphy.com/media/13d3TCCoxQFEeA/giphy.gif"           
-            imgJulia.attr("src",imgUrlMel);
-            nameJulia.html('Melanie West');
+            imgJulia.attr("src",imgUrlJul);
+            nameJulia.html('Julia McGee');
             
             $('#Julia_McGee').append(imgJulia);
             $('#Julia_McGee').append(nameJulia);
