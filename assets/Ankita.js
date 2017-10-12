6806266693dfdedd2697a302fb87210b8ef079c3
Ankita Shah <ankitashah212@gmail.com>
2017-10-12 08:42:50 -0400

    Update Ankita.js

--- assets/Ankita.js
+++ assets/Ankita.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
-
+      var imgAnkita = $('<img>');
+            var nameAnkita = $('<p>');
+            var imgUrlKita = "https://media.giphy.com/media/bxeGwS4uKw7rW/giphy.gif"           
+            imgAnkita.attr("src",imgUrlKita);
+            nameAnkita.html('Ankita Shah');
+            
+            $('#Ankita_Shah').append(imgAnkita);
+            $('#Ankita_Shah').append(nameAnkita);
 
 });
