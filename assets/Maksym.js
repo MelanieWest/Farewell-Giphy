a201afc128533e80360fff672363b23968a441d0
myskam7 <mksmflppv98@gmail.com>
2017-10-13 11:39:06 -0400

    maksym

---
+++ assets/Maksym.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+     var imgMaksym = $('<img>');
+     var nameMaksym = $('<p>');
+     var imgUrlMel = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif"           
+     imgMelanie.attr("src",imgUrlMel);
+     nameMelanie.html('Maksym Filippov');
+     
+     $('#Maksym_Filippov').append(imgMaksym);
+     $('#Maksym_Filippov').append(nameMaksym);
+     
+});
