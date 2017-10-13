94a8689e0e9362c95e0b001624dda2d25e681690
Lou Canizzo <luca.canizzo@gmail.com>
2017-10-13 08:59:49 -0400

    Update Luca.js

---
+++ assets/Luca.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+           
+            var imgLuca = $('<img>');
+            var nameLuca = $('<p>');
+            var imgUrlLou = "https://media0.giphy.com/media/5wWf7H89PisM6An8UAU/giphy.gif"           
+            imgLuca.attr("src",imgUrlLou);
+            nameLou.html('Luca Canizzo');
+            
+            $('#Luca_Canizzo').append(imgLuca);
+            $('#Luca_Canizzo').append(nameLuca);
+            
+});
