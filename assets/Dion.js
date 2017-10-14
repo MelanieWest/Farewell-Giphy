e4901e4f413ce5b2ca255b02780982dd7412fd7e
dionCavanaugh <dioncavanaugh@hotmail.com>
2017-10-13 20:47:14 -0400

    added my pic

--- assets/Dion.js
+++ assets/Dion.js
@@ -2,11 +2,10 @@
 $(document).ready(function(){
     
      var imgDion = $('<img>');
-     var nameDion = $('<h3>');
+     var nameDion = $('<p>');
      var imgUrlDion = "https://media.giphy.com/media/kaBmqpJtdvFqo/giphy.gif"           
      imgDion.attr("src",imgUrlDion);
      nameDion.html('Dion Cavanaugh');
-     
      $('#Dion_Cavanaugh').append(imgDion);
      $('#Dion_Cavanaugh').append(nameDion);
      
