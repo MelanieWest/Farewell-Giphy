3dbdb173d09c37ee013e91ef3ab225f106eb8f91
Melanie West <dhmofan@gmail.com>
2017-10-15 09:29:26 -0400

    Fix my typo of Maxym's name. Not sure why Jose's image won't show - all seems fine

--- assets/Jose.js
+++ assets/Jose.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
             var imgJose = $('<img>');
             var nameJose = $('<p>');
             var imgUrlJose = "https://tenor.com/view/method-man-soul-plane-sliding-in-sliding-in-the-dms-gif-5595353"
-            //var imgUrlJose = "https://tenor.com/view/method-man-soul-plane-sliding-in-sliding-in-the-dms-gif-5595353"           
             imgJose.attr("src",imgUrlJose);
             nameJose.html('Jose Guzman');
             
