8d9952e04217b5d979e567ffa65a3be83af38564
Melanie West <dhmofan@gmail.com>
2017-10-15 09:56:24 -0400

    update one image and remove comments

--- assets/Francisco.js
+++ assets/Francisco.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
     
      var imgFran = $('<img>');
      var nameFran = $('<p>');
-    // https://media.giphy.com/media/Hl1PqpoxTVGdG/giphy.gif
-     var imgUrlMel = "https://media.giphy.com/media/4Q0sAvf7RRT8I/giphy.gif"           
-     //var imgUrlMel = "https://media.giphy.com/media/g9582DNuQppxC/giphy.gif"
-     imgFran.attr("src",imgUrlMel);
+     var imgUrlFC = "https://media.giphy.com/media/4Q0sAvf7RRT8I/giphy.gif";           
+     imgFran.attr("src",imgUrlFC);
      nameFran.html('Francisco Carrera');
      
      $('#Francisco_Carrera').append(imgFran);
--- assets/Maksym.js
+++ assets/Maksym.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
      var imgMaksym = $('<img>');
      var nameMaksym = $('<p>');
-     var imgUrlMax = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif"           
+     var imgUrlMax = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif";           
      imgMaksym.attr("src",imgUrlMax);
      nameMaksym.html('Maksym Filippov');
      
