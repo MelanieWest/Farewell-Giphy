450354afa200b7467ef6a3c36a9851d23f7115fa
Melanie West <dhmofan@gmail.com>
2017-10-15 12:06:32 -0400

    copy link from giphy again

--- assets/Maksym.js
+++ assets/Maksym.js
@@ -2,14 +2,13 @@ $(document).ready(function(){
     
      var imgMaksym = $('<img>');
      var nameMaksym = $('<p>');
-
-     var imgUrlMaks = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif";           
+     var imgUrlMaks = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif"
      imgMaksym.attr("src",imgUrlMaks);
      nameMaksym.html('Maksym Filippov');
      
-    //  $('#Maksym_Filippov').append(imgMaksym);
-    //  $('#Maksym_Filippov').append(nameMaksym);
-     $('#open').append(imgMaksym);
-     $('#open').append(nameMaksym);
+     $('#Maksym_Filippov').append(imgMaksym);
+     $('#Maksym_Filippov').append(nameMaksym);
+    //  $('#open').append(imgMaksym);
+    //  $('#open').append(nameMaksym);
      
 });
