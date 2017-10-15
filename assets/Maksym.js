433772cdd6dc570f430f627e2a19def2a1f15ddd
Melanie West <dhmofan@gmail.com>
2017-10-15 11:30:25 -0400

    get maksym's image to show

--- assets/Maksym.js
+++ assets/Maksym.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
      var imgMaksym = $('<img>');
      var nameMaksym = $('<p>');
 
-     var imgUrlMax = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif";           
-     imgMaksym.attr("src",imgUrlMax);
+     var imgUrlMaks = "https://media.giphy.com/media/5aLrlDiJPMPFS/giphy.gif";           
+     imgMaksym.attr("src",imgUrlMaks);
      nameMaksym.html('Maksym Filippov');
      
      $('#Maksym_Filippov').append(imgMaksym);
